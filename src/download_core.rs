ce83ae07d4d362c3ab6e81291b32330687d87ec8
AlphaQian <154665575+Alpha-Qian@users.noreply.github.com>
2025-05-10 10:38:19 +0800

    添加write_once_to_end函数

--- src/download_core.rs
+++ src/download_core.rs
@@ -1,6 +1,6 @@
 //use std::io::SeekFrom;
 use std::{future::Future, io::SeekFrom, mem::{transmute,MaybeUninit}, os::windows::process, sync::{atomic::{AtomicU16, AtomicU64, AtomicU8, Ordering}, Arc}, time::Duration};
-use thiserror::Error;
+use std::ops::ControlFlow;
 use std::ptr::NonNull;
 use headers::{self,
     HeaderMapExt,
@@ -77,23 +77,42 @@ pub(crate) async unsafe fn download_once(
     Ok(())
 }
 
+#[inline]
+pub(crate) async fn write_once_to_end(
+    chunk: &[u8],
+    writer: &mut impl Writer,
+    process_sync: &mut impl ProcessSync,
+    end_sync: &mut impl EndSync,
+) -> DownloadResult<ControlFlow<()>> 
+{
+    if let Some(end) = end_sync.get_end().await {
+        let process = process_sync.get_process().await;
+        if process + chunk_size as u64 > end {
+            writer.write_all(&chunk[..(end - process) as usize]).await?;
+            process_sync.fetch_add((end - process) as u32).await;
+            return Ok(ControlFlow::Break(()));
+        };
+    };
+
+    writer.write_all(chunk.as_ref()).await?;
+    process_sync.fetch_add(chunk_size as u32).await;
+    Ok(ControlFlow::Continue(()))
+}
+
 pub(crate) async unsafe fn download_once_unrangeable(
     stream: &mut (impl StreamExt<Item = Result<Bytes, reqwest::Error>> + std::marker::Unpin),
     writer: &mut impl Writer,
 
     download_process_sync: &mut impl ProcessSync,
-    writed_process_sync: &mut impl ProcessSync,//?
-    end_sync: &mut impl EndSync,//因为不能续传，似乎不需要EndSync trait
+    writed_process: &mut u64,
+    end_sync: &u64,
 
-    // build_request: impl FnOnce(&mut HeaderMap, &mut Option<Duration>, &mut Option<Version>),
-    // check_response: impl FnOnce(&Response) -> Result<CheckType, DownloadError>,
-    // reciving_guard: impl FnOnce() -> impl Future<Output=Result<()>>,
-    // process_guard: impl FnOnce() -> impl Future<Output=Result<()>>,
 ) -> DownloadResult<()> 
 {
     while let Some(item) = stream.next().await{
         let chunk = item?;
         let chunk_size = chunk.len();
+
     }
 
     while let Some(item) = stream.next().await{
